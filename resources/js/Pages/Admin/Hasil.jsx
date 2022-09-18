d07d4b8eef4098e94075e35a7c1c70199618849d
eganproject <eganugrahaid@gmail.com>
2022-09-18 09:38:40 +0700

    nambah file title

--- resources/js/Pages/Admin/Hasil.jsx
+++ resources/js/Pages/Admin/Hasil.jsx
@@ -12,7 +12,7 @@ const Hasil = (props) => {
     errors={props.errors}
     header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Perhitungan</h2>}
     >
-        <Head title="Perhitungan" />
+        <Head title="Hasil" />
 
 {pengingatnya ?  
             <>
